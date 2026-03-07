c720f018e5552801fca38799ee9166823001ccdf
Nur Rachmat <rachmat.nur91@gmail.com>
2026-03-07 01:26:50 +0000

    Implement main navigation and add search functionality with movie details

--- pilem/lib/screens/home_screen.dart
+++ pilem/lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pilem/models/movie.dart';
+import 'package:pilem/screens/detail_screen.dart';
 import 'package:pilem/services/api_services.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -17,19 +18,19 @@ class _HomeScreenState extends State<HomeScreen> {
   List<Movie> _popularMovies = [];
 
   Future<void> _loadMovies() async {
-    final List<Map<String, dynamic>> allMoviesData = await _apiService
-        .getAllMovies();
-    final List<Map<String, dynamic>> trendingMoviesData = await _apiService
-        .getTrendingMovies();
-    final List<Map<String, dynamic>> popularMoviesData = await _apiService
-        .getPopularMovies();
+    final List<Map<String, dynamic>> allMoviesData =
+        await _apiService.getAllMovies();
+    final List<Map<String, dynamic>> trendingMoviesData =
+        await _apiService.getTrendingMovies();
+    final List<Map<String, dynamic>> popularMoviesData =
+        await _apiService.getPopularMovies();
 
     setState(() {
-      _allMovies = allMoviesData.map((e) => Movie.fromJson(e)).toList();
-      _trendingMovies = trendingMoviesData
-          .map((e) => Movie.fromJson(e))
-          .toList();
-      _popularMovies = popularMoviesData.map((e) => Movie.fromJson(e)).toList();
+      _allMovies = allMoviesData.map((json) => Movie.fromJson(json)).toList();
+      _trendingMovies =
+          trendingMoviesData.map((json) => Movie.fromJson(json)).toList();
+      _popularMovies =
+          popularMoviesData.map((json) => Movie.fromJson(json)).toList();
     });
   }
 
@@ -42,61 +43,76 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Pilem")),
+      appBar: AppBar(
+        title: const Text("Pilem"),
+      ),
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            _buildMovieList("All Movies", _allMovies),
-            _buildMovieList("Trending Movies", _trendingMovies),
-            _buildMovieList("Popular Movies", _popularMovies),
+            _buildMoviesList("All Movies", _allMovies),
+            _buildMoviesList("Trending Movies", _trendingMovies),
+            _buildMoviesList("Popular Movies", _popularMovies),
           ],
         ),
       ),
     );
   }
 
-  Widget _buildMovieList(String title, List<Movie> movies) {
+  Widget _buildMoviesList(String title, List<Movie> movies) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
+        // Menampilkan Title Kategori Movies
         Padding(
-          padding: EdgeInsets.all(8.0),
+          padding: const EdgeInsets.all(8.0),
           child: Text(
             title,
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            style: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
           ),
         ),
+        //Menapilkan thumnail dan judul movies
         SizedBox(
           height: 200,
           child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemCount: movies.length,
-            itemBuilder: (BuildContext context, int index) {
-              final Movie movie = movies[index];
-              return Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Column(
-                  children: [
-                    Image.network(
-                      'https://image.tmdb.org/t/p/w500${movie.posterPath}',
-                      height: 150,
-                      width: 100,
-                      fit: BoxFit.cover,
+              scrollDirection: Axis.horizontal,
+              itemCount: movies.length,
+              itemBuilder: (BuildContext build, int index) {
+                final Movie movie = movies[index];
+                return GestureDetector(
+                  onTap: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => DetailScreen(movie: movie),
                     ),
-                    SizedBox(height: 5),
-                    Text(
-                      movie.title,
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                      ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Image.network(
+                          "https://image.tmdb.org/t/p/w500${movie.posterPath}",
+                          width: 100,
+                          height: 150,
+                          fit: BoxFit.cover,
+                        ),
+                        Text(
+                          movie.title.length > 14
+                              ? '${movie.title.substring(0, 10)}...'
+                              : movie.title,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-              );
-            },
-          ),
-        ),
+                  ),
+                );
+              }),
+        )
       ],
     );
   }
