c8ad169b5874c254bfdb56711372919b24a97fb9
AndhikaRCP <rachmat.nur91@gmail.com>
2026-05-06 13:09:53 +0700

    Convert MapDetailScreen to StatefulWidget

--- cepu_app/lib/screens/map_detail_screen.dart
+++ cepu_app/lib/screens/map_detail_screen.dart
@@ -4,17 +4,23 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 
 //install depenencies flutter_map dan latlong2
-class MapDetailScreen extends StatelessWidget {
+class MapDetailScreen extends StatefulWidget {
   final Post post;
   const MapDetailScreen({super.key, required this.post});
+
+  @override
+  State<MapDetailScreen> createState() => _MapDetailScreenState();
+}
+
+class _MapDetailScreenState extends State<MapDetailScreen> {
   @override
   Widget build(BuildContext context) {
-    final lat = double.tryParse(post.latitude ?? '');
-    final lng = double.tryParse(post.longitude ?? '');
+    final lat = double.tryParse(widget.post.latitude ?? '');
+    final lng = double.tryParse(widget.post.longitude ?? '');
     final hasLocation = lat != null && lng != null;
     final point = hasLocation ? LatLng(lat, lng) : const LatLng(0, 0);
     return Scaffold(
-      appBar: AppBar(title: Text(post.category ?? 'Map Detail')),
+      appBar: AppBar(title: Text(widget.post.category ?? 'Map Detail')),
       body: hasLocation
           ? FlutterMap(
               options: MapOptions(initialCenter: point, initialZoom: 15),
